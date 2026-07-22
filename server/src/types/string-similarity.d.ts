f500e2f92344b1de06145c5930cf3f3f0afacb6d
Tummala <tummalaakshaya070@gmail.com>
2026-07-22 21:10:03 +0530

    fix: add string-similarity declaration

--- server/src/types/string-similarity.d.ts
+++ server/src/types/string-similarity.d.ts
@@ -1,1 +1,22 @@
-declare module "string-similarity";
+declare module "string-similarity" {
+  export interface Match {
+    target: string;
+    rating: number;
+  }
+
+  export interface BestMatchResult {
+    ratings: Match[];
+    bestMatch: Match;
+    bestMatchIndex: number;
+  }
+
+  const stringSimilarity: {
+    compareTwoStrings(first: string, second: string): number;
+    findBestMatch(
+      mainString: string,
+      targetStrings: string[]
+    ): BestMatchResult;
+  };
+
+  export default stringSimilarity;
+}
